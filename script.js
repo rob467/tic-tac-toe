92126ba700003cfd957f25dd0fdac28c609a4d5c
rob467 <133133177+rob467@users.noreply.github.com>
2025-01-07 16:37:39 +0100

    Tidied up game winning logic
    Put game winning logic into single function and
    added logic for a tie. Initialised DOM
    functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. Set up 3 by 3 gameboard array in gameboard object
+// Set up 3 by 3 gameboard array in gameboard object
 function Gameboard() {
     const rows = 3;
     const columns = 3;
@@ -63,6 +63,7 @@ function gameController() {
     ];
 
     let activePlayer = players[0]
+    let gameOver = false
 
     const switchPlayer = () =>
         activePlayer = activePlayer === players[0] ? players[1] : players[0]
@@ -72,10 +73,16 @@ function gameController() {
 
     const printNewTurn = () => {
         board.printBoard();
+        if (gameOver) {
+            return;
+        }
         console.log(`${getActivePlayer().name}'s turn!`)
     }
 
     const makeTurn = (row, column) => {
+        if (gameOver) {
+            return;
+        }
         if (board.getBoard()[row][column].getValue() !== 0) {
             console.log("This square is taken! Please select a different square.");
             printNewTurn();
@@ -84,38 +91,43 @@ function gameController() {
         console.log(`${getActivePlayer().name} goes in row ${row}, column ${column}`)
         board.selectCell(getActivePlayer().token, row, column)
 
-        // Check rows:
-        const checkWinner = (board) => {
-            for (const row of board) {
-                if (row.every(val => val === row[0] && val !== 0)){
-                    console.log(`Game Over! ${getActivePlayer().name} wins!`)
-                    return;
-                }
+        // Check if game has been won:
+        const checkWinner = () => {
+            boardValues = board.getBoardValues()
+            
+            if (boardValues.every(row => row.every(val => val !==0))) {
+                console.log(`Game Over! Game is tied!`)
+                gameOver = true;
             }
-        };
 
-        // get transposed array to check columns
-        const getTransposedBoard = () => {
-            let colArray = board.getBoardValues()[0].map(
-                (_, colIndex) => board.getBoardValues().map(row => row[colIndex]))
-            return colArray;
-        };
+            let transposedBoard = boardValues[0].map(
+                (_, colIndex) => boardValues.map(row => row[colIndex]))
 
-        // check diagonal
-        const diagonalArrays = () => {
+            
             let firstDiagonal = []
             let secondDiagonal = []
-            for (let i = 0; i < board.getBoardValues().length; i++) {
-                firstDiagonal.push(board.getBoardValues()[i][i])
-                secondDiagonal.push(board.getBoardValues()[i][board.getBoardValues().length - 1 - i])
+            for (let i = 0; i < boardValues.length; i++) {
+                firstDiagonal.push(boardValues[i][i])
+                secondDiagonal.push(boardValues[i][boardValues.length - 1 - i])
             }
-            return [firstDiagonal, secondDiagonal];
+            function checkWinnerDirection(arrValues) {
+                for (const row of arrValues) {
+                    if (row.every(val => val === row[0] && val !== 0)){
+                        console.log(`Game Over! ${getActivePlayer().name} wins!`)
+                        gameOver = true;
+                        return;
+                    }
+                }
             }
+
+            checkWinnerDirection(boardValues)
+            checkWinnerDirection(transposedBoard)
+            checkWinnerDirection([firstDiagonal, secondDiagonal])
+            
+        };
+
         
-        console.log(diagonalArrays())
-        checkWinner(board.getBoardValues())
-        checkWinner(getTransposedBoard())
-        checkWinner(diagonalArrays())
+        checkWinner()
         switchPlayer();
         printNewTurn();
     }
@@ -125,11 +137,20 @@ function gameController() {
     return { makeTurn, getActivePlayer, switchPlayer };
 }
 
-const game = gameController();
-
-// Declare winner (or tie) when game extends
 // Display gameboard in DOM
+function displayGameboard () {
+    const gameboardDisplay = document.querySelector(".gameboard")
+
+    const board = getBoard();
+
+    for (const row of board) {
+        
+    }
+}
+
 // Create functions to allow user to click square for Selection
 // Allow users to input and display user names
 // Add start/restart game buttons
 // Display game result at end
+
+const game = gameController();
