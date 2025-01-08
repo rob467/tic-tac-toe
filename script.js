fc7450e8da7dc06c2884e74cf28408f8c7fba06b
rob467 <133133177+rob467@users.noreply.github.com>
2025-01-08 16:26:43 +0100

    Added board display to DOM

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function Gameboard() {
     const rows = 3;
     const columns = 3;
+    const gameboardDisplay = document.querySelector(".gameboard")
+
     const board = [];
     for (let i = 0; i < rows; i++) {
         board[i] = [];
@@ -20,16 +22,32 @@ function Gameboard() {
     const getBoardValues = () => board.map((row) =>
         row.map((cell) => cell.getValue()))
 
+    function displayCell (cellValue) {
+        const cellDiv = document.createElement("div")
+        cellDiv.setAttribute("class", "cell")
+        cellDiv.textContent = `${cellValue}`
+        gameboardDisplay.appendChild(cellDiv)
+    }
+
+    function displayBoard () {
+        while (gameboardDisplay.firstChild) {
+            gameboardDisplay.removeChild(gameboardDisplay.firstChild);
+        }
+        let boardValues = getBoardValues();
+        boardValues.map((row) => 
+        row.map((cell) => displayCell(cell)))
+    }
+
     const printBoard = () => {
         console.log(getBoardValues())
     }
 
-    return { getBoard, selectCell, printBoard, getBoardValues }
+    return { getBoard, selectCell, printBoard, getBoardValues, displayCell, displayBoard }
 
 }
 
 function Cell() {
-    let value = 0
+    let value = ""
 
     const addSelection = (player) => {
         value = player;
@@ -46,19 +64,24 @@ function Cell() {
 // Create game object to track the game, including player objects
 function gameController() {
 
+    const gameDetailsDiv = document.querySelector(".game-details")
+    const playersTurnDiv = document.querySelector(".players-turn");
+    const gameStateDiv = document.querySelector(".game-state");
+
     let playerOneName = "Player 1"
     let playerTwoName = "Player 2"
 
     const board = Gameboard();
+    // board.displayBoard();
     
     const players = [
         {
             name: playerOneName,
-            token: 1
+            token: "X"
         },
         {
             name: playerTwoName,
-            token: 2
+            token: "O"
         }
     ];
 
@@ -72,32 +95,39 @@ function gameController() {
     const getActivePlayer = () => activePlayer;
 
     const printNewTurn = () => {
+        board.displayBoard();
         board.printBoard();
-        if (gameOver) {
-            return;
-        }
         console.log(`${getActivePlayer().name}'s turn!`)
+        playersTurnDiv.textContent = `${getActivePlayer().name}'s turn!`
     }
 
-    const makeTurn = (row, column) => {
-        if (gameOver) {
+    const gameOverTest = () =>
+        {if (gameOver) {
+            gameDetailsDiv.removeChild(playersTurnDiv)
             return;
-        }
-        if (board.getBoard()[row][column].getValue() !== 0) {
+        }}
+
+    const makeTurn = (row, column) => {
+
+        if (board.getBoard()[row][column].getValue() !== "") {
             console.log("This square is taken! Please select a different square.");
+            gameStateDiv.textContent = "This square is taken! Please select a different square.";
             printNewTurn();
             return;
         }
-        console.log(`${getActivePlayer().name} goes in row ${row}, column ${column}`)
+        // console.log(`${getActivePlayer().name} goes in row ${row}, column ${column}`)
+        // gameStateDiv.textContent = `${getActivePlayer().name} goes in row ${row}, column ${column}`
         board.selectCell(getActivePlayer().token, row, column)
 
         // Check if game has been won:
         const checkWinner = () => {
-            boardValues = board.getBoardValues()
+            boardValues = board.getBoardValues();
             
-            if (boardValues.every(row => row.every(val => val !==0))) {
+            if (boardValues.every(row => row.every(val => val !== ""))) {
                 console.log(`Game Over! Game is tied!`)
+                gameStateDiv.textContent = `Game Over! Game is tied!`
                 gameOver = true;
+                gameOverTest();
             }
 
             let transposedBoard = boardValues[0].map(
@@ -112,9 +142,11 @@ function gameController() {
             }
             function checkWinnerDirection(arrValues) {
                 for (const row of arrValues) {
-                    if (row.every(val => val === row[0] && val !== 0)){
+                    if (row.every(val => val === row[0] && val !== "")){
                         console.log(`Game Over! ${getActivePlayer().name} wins!`)
+                        gameStateDiv.textContent = `Game Over! ${getActivePlayer().name} wins!`
                         gameOver = true;
+                        gameOverTest();
                         return;
                     }
                 }
@@ -123,30 +155,65 @@ function gameController() {
             checkWinnerDirection(boardValues)
             checkWinnerDirection(transposedBoard)
             checkWinnerDirection([firstDiagonal, secondDiagonal])
-            
         };
 
         
         checkWinner()
         switchPlayer();
         printNewTurn();
+        // displaySelection();
     }
 
     printNewTurn();
-
-    return { makeTurn, getActivePlayer, switchPlayer };
-}
-
-// Display gameboard in DOM
-function displayGameboard () {
-    const gameboardDisplay = document.querySelector(".gameboard")
-
-    const board = getBoard();
-
-    for (const row of board) {
+    // Display gameboard in DOM
+    function displaySelection () {
+        // const cells = document.querySelectorAll(".cell")
         
+        // let cellIds = Array.from(cells).map(cell => cell.getAttribute("id").split("-"))
+        // console.log(cellIds)
+
+        
+        
+        boardValues = board.getBoardValues();
+        // boardValues.forEach(row => {
+        //     cellIds.forEach(cell => console.log(cell[0]))})
+
+        // boardValues.forEach(row => {
+        //     row.forEach(
+        //         cell => {
+        //             if (cell === 0) {
+        //                 gameboardDisplay.textContent = " "
+        //                 console.log("empty cell")
+        //             } else if (cell === 1) {
+        //                 gameboardDisplay.textContent = "X"
+        //                 console.log("X")
+        //             } else {
+        //                 gameboardDisplay.textContent = "O"
+        //                 console.log("O")
+        //             }
+        //         }
+        //     )
+        // });
+
+            // cells.forEach(
+            //     cell => cell.addEventListener("click", 
+            //         () => playerText(cell)
+            //     ))
+        
+        function playerText (cell) {
+            if (activePlayer.token === 1) {
+                cell.textContent = "X"
+            } else {
+                cell.textContent = "O"
+            }
+        }
+
+
     }
+
+    return { makeTurn, getActivePlayer, switchPlayer, displaySelection };
 }
+
 
 // Create functions to allow user to click square for Selection
 // Allow users to input and display user names
